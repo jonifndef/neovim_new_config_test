0fdb79dc7287fe3cd5249f3c0e366cc936647fd9
Jonas Fredriksson <jonas.fredriksson@combitech.com>
2025-09-22 08:47:57 +0200

    Add treesitter and plenary

--- neovim_test_config/config/lua/configs/pack.lua
+++ neovim_test_config/config/lua/configs/pack.lua
@@ -1,3 +1,6 @@
 vim.pack.add({
     { src = 'https://github.com/ellisonleao/gruvbox.nvim.git' },
+    { src = 'https://github.com/nvim-lua/plenary.nvim.git' },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = 'main' },
+    --{ src = 'https://github.com/nvim-telescope/telescope.nvim.git', version = '0.1.8' },
 })
